6775930c402c899e6da3777621880a220705ee99
lucaasemanueel <lucas.emanuel@dce.ufpb.br>
2021-08-31 14:02:02 -0300

    Conclusão do script deletarPet e finalização do crud de automacao

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -19,7 +19,7 @@ public class Pet {
                 return new String(Files.readAllBytes(Paths.get(caminhoJson)));
         }
 
-        @Test (priority = 2)
+        @Test (priority = 1)
         public void incluirPet() throws IOException {
                 String jsonBody = lerJson("db/pet1.json");
 
@@ -38,7 +38,7 @@ public class Pet {
                         .body("tags.name", contains("data"));
         }
 
-        @Test (priority = 1)
+        @Test (priority = 2)
         public void consultarPet(){
                 String petId = "2004199649";
 
@@ -61,5 +61,39 @@ public class Pet {
 
                 System.out.println("O token é " + token);
 
+        }
+
+        @Test (priority = 3)
+        public void alterarPet() throws IOException {
+                String jsonBody = lerJson("db/pet2.json");
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                        .body(jsonBody)
+                .when()
+                        .put(uri)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("name", is("Atena"))
+                        .body("status", is("Vendido"))
+                ;
+        }
+
+        @Test (priority =  4)
+        public void deletarPet(){
+                String petId = "2004199649";
+
+                given()
+                        .contentType("application/json")
+                        .log().all()
+                .when()
+                        .delete(uri + "/" + petId)
+                .then()
+                        .log().all()
+                        .statusCode(200)
+                        .body("code", is(200))
+                        .body("type", is ("unknown"))
+                        .body("message", is(petId));
         }
 }
